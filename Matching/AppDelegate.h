e29b60a99ac05f2b8a03ded35bd62f2cce97af28
[David Anglin] <[DAAnglin6389@gmail.com]>
2016-03-08 14:30:36 -0800

    initial commit

---
+++ Matching/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matching
+//
+//  Created by David Anglin on 3/8/16.
+//  Copyright © 2016 David Anglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
