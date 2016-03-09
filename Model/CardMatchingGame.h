ec018d95a4e56d347c1cf797a542ae93f391878f
[David Anglin] <[DAAnglin6389@gmail.com]>
2016-03-09 13:19:57 -0800

    added more cards to screen, matching functionality working, keeping score

---
+++ Model/CardMatchingGame.h
@@ -0,0 +1,24 @@
+//
+//  CardMatchingGame.h
+//  Matching
+//
+//  Created by David Anglin on 3/9/16.
+//  Copyright © 2016 David Anglin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+//designated initializer
+- (instancetype) initWithCardCount: (NSUInteger)count
+                         usingDeck: (Deck *) deck;
+
+- (void)chooseCardAtIndex: (NSUInteger) index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) NSInteger score;
+
+
+@end
